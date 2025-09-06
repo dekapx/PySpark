2a6ac22c3543585790a292ce72f1e70deb515e1e
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2025-09-06 23:12:51 +0100

    apache spark java samples

--- pyspark-project/src/chapter01/csv_read.py
+++ pyspark-project/src/chapter01/csv_read.py
@@ -1,5 +1,5 @@
-# generate code to read a CSV file and print its contents
 import csv
+
 with open('src/chapter01/data.csv', mode='r') as file:
     reader = csv.reader(file)
     for row in reader:
